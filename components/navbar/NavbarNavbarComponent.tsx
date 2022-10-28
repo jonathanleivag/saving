2cced9d31ced774ff38e66e488bdc38cd3f96f68
jonathanleivag <email@jonathanleivag.cl>
2022-10-27 21:00:40 -0300

    navbar

--- components/navbar/NavbarNavbarComponent.tsx
+++ components/navbar/NavbarNavbarComponent.tsx
@@ -1,10 +1,22 @@
-import { Text, View } from 'native-base'
+import { Flex, Image, Text } from 'native-base'
 
 const NavbarNavbarComponent = () => {
   return (
-    <View>
-      <Text>NavbarNavbarComponent</Text>
-    </View>
+    <Flex direction='row' alignItems={'center'} w='full' h='16'>
+      <Flex direction='row' alignItems='center' h='full'>
+        <Image
+          source={{
+            uri:
+              'https://www.jonathanleivag.cl/_next/image?url=%2Flogo%2FlogoColor.png&w=256&q=75'
+          }}
+          alt='Logo de jonathanleivag'
+          size='sm'
+        />
+        <Text fontFamily='header' fontSize='2xl' marginLeft={2}>
+          Saving
+        </Text>
+      </Flex>
+    </Flex>
   )
 }
 
