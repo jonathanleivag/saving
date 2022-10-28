8be76261edce84eff2d87ae1fa374cfd99302fdb
jonathanleivag <email@jonathanleivag.cl>
2022-10-28 00:44:04 -0300

    saque el componente box de StaggerStaggerComponent

--- components/stagger/StaggerStaggerComponent.tsx
+++ components/stagger/StaggerStaggerComponent.tsx
@@ -1,6 +1,5 @@
 import { Entypo, MaterialCommunityIcons } from '@expo/vector-icons'
 import {
-  Box,
   HStack,
   Icon,
   IconButton,
@@ -25,82 +24,80 @@ const StaggerStaggerComponent: FC<IStaggerComponentProps> = ({ setCash }) => {
 
   return (
     <View style={styles.container}>
-      <Box>
-        <Stagger
-          visible={isOpen}
-          initial={{
-            opacity: 0,
-            scale: 0,
-            translateY: 34
-          }}
-          animate={{
-            translateY: 0,
-            scale: 1,
-            opacity: 1,
-            transition: {
-              type: 'spring',
-              mass: 0.8,
-              stagger: {
-                offset: 30,
-                reverse: true
-              }
+      <Stagger
+        visible={isOpen}
+        initial={{
+          opacity: 0,
+          scale: 0,
+          translateY: 34
+        }}
+        animate={{
+          translateY: 0,
+          scale: 1,
+          opacity: 1,
+          transition: {
+            type: 'spring',
+            mass: 0.8,
+            stagger: {
+              offset: 30,
+              reverse: true
             }
-          }}
-          exit={{
-            translateY: 34,
-            scale: 0.5,
-            opacity: 0,
-            transition: {
-              duration: 100,
-              stagger: {
-                offset: 30,
-                reverse: true
-              }
+          }
+        }}
+        exit={{
+          translateY: 34,
+          scale: 0.5,
+          opacity: 0,
+          transition: {
+            duration: 100,
+            stagger: {
+              offset: 30,
+              reverse: true
             }
-          }}
-        >
-          <IconButton
-            mb='4'
-            variant='solid'
-            size='lg'
-            bg='secondary.500'
-            colorScheme='red'
-            borderRadius='full'
-            onPress={handleSetCash}
-            icon={
-              <Icon
-                as={MaterialCommunityIcons}
-                size='10'
-                name='cash-plus'
-                _dark={{
-                  color: 'white'
-                }}
-                color='white'
-              />
-            }
-          />
-        </Stagger>
-        <HStack>
-          <IconButton
-            variant='solid'
-            borderRadius='full'
-            size='lg'
-            onPress={onToggle}
-            bg='cyan.400'
-            icon={
-              <Icon
-                as={Entypo}
-                size='10'
-                name='plus'
-                color='white'
-                _dark={{
-                  color: 'warmGray.50'
-                }}
-              />
-            }
-          />
-        </HStack>
-      </Box>
+          }
+        }}
+      >
+        <IconButton
+          mb='4'
+          variant='solid'
+          size='lg'
+          bg='secondary.500'
+          colorScheme='red'
+          borderRadius='full'
+          onPress={handleSetCash}
+          icon={
+            <Icon
+              as={MaterialCommunityIcons}
+              size='10'
+              name='cash-plus'
+              _dark={{
+                color: 'white'
+              }}
+              color='white'
+            />
+          }
+        />
+      </Stagger>
+      <HStack>
+        <IconButton
+          variant='solid'
+          borderRadius='full'
+          size='lg'
+          onPress={onToggle}
+          bg='cyan.400'
+          icon={
+            <Icon
+              as={Entypo}
+              size='10'
+              name='plus'
+              color='white'
+              _dark={{
+                color: 'warmGray.50'
+              }}
+            />
+          }
+        />
+      </HStack>
     </View>
   )
 }
